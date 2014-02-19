1bf118c97892fa6139e2df60f3fcba2abb464bbb
PeppyHare <bluhme3@gmail.com>
2014-02-18 22:45:35 -0500

    Deleting cruft

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/evan/Documents/EasyWand
-# Build directory: /Users/evan/Documents/EasyWand
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(StillRuns "exec" "RGBsetup_profile.txt" "wandpts.csv" "bkgdPts.csv")
